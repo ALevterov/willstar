341591c0d255a30d795d0f632ac9aad64b65993c
Alexander Levterov <shuroffrashiday@yandex.ru>
2023-03-25 12:18:49 +0300

    добавлена очень простая валидация формы

--- js/main.js
+++ js/main.js
@@ -405,3 +405,44 @@ setInterval(() => {
     runningStringInner.classList.add('animated2')
   }, 600)
 }, 4000)
+
+let nameInput = document.querySelector('input[name="name"]')
+let nameLabel = document.querySelector('label[for="name"]')
+let emailInput = document.querySelector('input[name="email"]')
+let emailLabel = document.querySelector('label[for="email"]')
+
+let submitBtn = document.getElementById('submit')
+submitBtn.disabled = true
+
+function validateForm() {
+  console.log('validateForm')
+  if (nameInput.value === '' || emailInput.value === '') {
+    submitBtn.disabled = true
+  }
+
+  if (nameInput.value !== '' && emailInput.value !== '') {
+    submitBtn.disabled = false
+  }
+
+  if (nameInput.value === '') {
+    nameInput.style.borderBottom = '1px solid red'
+    nameLabel.style.color = 'red'
+  } else {
+    nameInput.style.borderBottom = '1px solid #fff'
+    nameLabel.style.color = '#fff'
+  }
+
+  if (emailInput.value === '') {
+    emailInput.style.borderBottom = '1px solid red'
+    emailLabel.style.color = 'red'
+  } else {
+    emailInput.style.borderBottom = '1px solid #fff'
+    emailLabel.style.color = '#fff'
+  }
+}
+
+nameInput.addEventListener('change', validateForm)
+nameInput.addEventListener('input', validateForm)
+
+emailInput.addEventListener('change', validateForm)
+emailInput.addEventListener('input', validateForm)
